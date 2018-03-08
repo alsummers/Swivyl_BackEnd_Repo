b5afce0a532f77b16b174512e0082875ab53075b
vmunson <virginia.munson@gmail.com>
2018-03-08 10:03:46 -0500

    added log model

---
+++ models/log.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var users = sequelize.define('log', {
+    id:{
+      type: DataTypes.INTEGER,
+      unique: true,
+      primaryKey: true
+    },
+    dateCreated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    companyID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return log;
+};
