e718ca9859012a9a350fd03a31b2dc0b295f434f
Dre Martin <anrumara@gmail.com>
2018-03-08 10:11:31 -0500

    added to do model

---
+++ models/todo.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var todo = sequelize.define('todo', {
+        companyID: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dateDue: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    }, {
+            classMethods: {
+                associate: function (models) {
+                    // associations can be defined here
+                }
+            }
+        });
+
+    return users;
+};
