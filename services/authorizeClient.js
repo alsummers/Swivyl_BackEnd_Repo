feb56559ab25e4cd82034cd0d246fceefa1a9251
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-03-12 15:50:31 -0400

    added env

--- services/authorizeClient.js
+++ services/authorizeClient.js
@@ -3,10 +3,10 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const db = require('../models/index').sequelize; 
 const Client = db.import('../models/client.js');
-
+require('dotenv').config()
 
 passport.use(new JwtStrategy(
-    {jwtFromRequest: ExtractJwt.fromHeader('authorization'), secretOrKey:'i_am_secret' },
+    {jwtFromRequest: ExtractJwt.fromHeader('authorization'), secretOrKey: process.env.JWTSECRET },
     (payload, done) => {
         Client.findOne({where: {uid:payload.sub}}).then(
             (client) => {
