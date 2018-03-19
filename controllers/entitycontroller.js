b4b667ff2b947bb3623e3dd1791d022a4339b68c
Allison Summers <allisonsummers44@gmail.com>
2018-03-19 13:53:38 -0400

    changed todo to DATEONLY

--- controllers/entitycontroller.js
+++ controllers/entitycontroller.js
@@ -10,8 +10,10 @@ const Log = db.sequelize.import('../models/log.js')
 // CREATING ENTITY
 router.post('/',requireJwt,(req, res)  => {
     var name = req.body.entity.name
-    var company = req.body.company.id
-	var owner = req.user.uid
+    var company = req.body.company.uid
+    var owner = req.user.uid
+    
+    
 
 
     Entity.create({
@@ -61,12 +63,12 @@ router.get('/all/:companyId' , requireJwt,function(req, res) {
 });
 
 //FINDING ONE SPECIFIC COMPANY
-router.get('/:id', requireJwt,function(req, res) {
-	var data = req.params.id;
-	// console.log(data); here for testing purposes
+router.get('/:uid', requireJwt,function(req, res) {
+	var data = req.params.uid;
+	console.log("GEEEEEEEEEET", data.uid);
 	Entity
 	.findOne({
-		where: {id: data}
+		where: {uid: data}
 	}).then(
 		function getSuccess(updateData) {
 			res.json(updateData);
@@ -80,7 +82,7 @@ router.get('/:id', requireJwt,function(req, res) {
 // UPDATING COMPANY
 router.put('/',requireJwt,(req, res)  => {
     var name = req.body.entity.name
-    var data = req.body.entity.id
+    var data = req.body.entity.uid
     var company = req.body.company.id
 	var owner = req.user.uid
 
@@ -110,12 +112,13 @@ router.put('/',requireJwt,(req, res)  => {
 });
 
 // DELETE SPECIFIC COMPANY
-router.delete('/:id',requireJwt, function(req, res) {
-	var data = req.params.id;
-	// console.log(data); here for testing purposes
+router.delete('/:uid',requireJwt, function(req, res) {
+    console.log("+++++++DATA+++++++", req.params)
+	var data = req.params.uid;
+    
 	Entity
 	.destroy({
-		where: {id: data}
+		where: {uid: data}
 	}).then(
 		function getSuccess(updateData) {
 			res.json(updateData);
