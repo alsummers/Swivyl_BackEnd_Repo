2d940a25fdaee930c284428edfb4fd604f690880
Allison Summers <allisonsummers44@gmail.com>
2018-03-20 11:47:47 -0400

    changed client find all and then some

--- router.js
+++ router.js
@@ -7,10 +7,18 @@ module.exports = app => {
     app.use('/api/fleet', require('./controllers/fleetcontroller'));
     app.use('/api/todo', require('./controllers/todocontroller'));
     app.use('/api/shareholders', require('./controllers/shareholderscontroller'));
+    const passport = require('passport');
 
 
     // Test page
     app.get('/', (req, res, next) => {
         res.sendFile(__dirname + '/index.html')
+    })
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile']
+    }));
+    
+    app.get('/auth/google/callback', passport.authenticate('google'), (req,res) => {
+        res.redirect('/')
     })
 }
